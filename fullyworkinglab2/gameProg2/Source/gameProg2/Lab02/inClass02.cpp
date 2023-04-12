575a59e6578e8a63b71d74d8a61ee1307c871735
Ryan Davidson <>
2023-04-12 17:54:56 -0600

    fully working lab 2 resurrected

---
+++ fullyworkinglab2/gameProg2/Source/gameProg2/Lab02/inClass02.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "inClass02.h"
+
+// Sets default values
+AinClass02::AinClass02()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    
+}
+
+// Called when the game starts or when spawned
+void AinClass02::BeginPlay()
+{
+    
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AinClass02::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
