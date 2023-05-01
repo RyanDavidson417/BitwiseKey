772272139648fbe141746e58dd249499e5098895
RyanDavidson417 <ryan.davidson.417@gmail.com>
2023-05-01 16:07:18 -0600

    removing secondary folders

--- InClassProject2/Source/InClassProject2/InClassProject2.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "InClassProject2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InClassProject2, "InClassProject2" );
- 
