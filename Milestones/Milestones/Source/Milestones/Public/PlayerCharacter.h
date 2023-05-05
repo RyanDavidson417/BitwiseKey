ba2bef3d03a557573f363ab3f9b140c2ea8980ca
Ryan Davidson <ryan.r.davidson@du.edu>
2023-05-05 16:32:22 -0600

    this is just a lot of testing - not sure why so many assets are changed

---
+++ Milestones/Milestones/Source/Milestones/Public/PlayerCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+class UInputComponent;
+class USkeletalMeshComponent;
+class USceneComponent;
+class UCameraComponent;
+class UAnimMontage;
+class USoundBase;
+
+
+UCLASS()
+class MILESTONES_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	//static mesh
+	UPROPERTY(VisibleDefaultsOnly, Category = mesh)
+	USkeletalMeshComponent* Mesh1P;
+
+	//actions
+	UPROPERTY(EditAnywhere)
+	 
+
+
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
