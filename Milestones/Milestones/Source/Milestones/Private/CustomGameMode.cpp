a7e13ff43c5bd9880480590b2e2504ab337d13f7
Ryan Davidson <ryan.davidson.417@gmail.com>
2023-05-18 14:06:22 -0600

    basic game mode and state infrastructure - still yet to define most of my rules

---
+++ Milestones/Milestones/Source/Milestones/Private/CustomGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomGameMode.h"
+
+ACustomGameMode::ACustomGameMode()
+{
+
+    /** initialize some of the defaults, blueprinting this will override it,
+        notice the use of StaticClass to get the UClass class type properly */
+    DefaultPawnClass = APlayerCharacter::StaticClass();
+    GameStateClass = ACustomGameState::StaticClass();
+}
---
+++ Milestones/Milestones/Source/Milestones/Public/CustomGameMode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PlayerCharacter.h"
+#include "CustomGameState.h"
+#include "CustomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MILESTONES_API ACustomGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ACustomGameMode();
+	
+};
